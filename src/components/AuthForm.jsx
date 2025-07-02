b35ff2189c6dcf16eb3baaa47135eb4b74fee33f
Benjamin Inglis <bjammin134@gmail.com>
2025-07-02 10:52:08 -0700

    Implement dynamic login/signup functionality

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,27 +1,39 @@
 import {
   useNavigate,
+  Link,
   Form,
   useNavigation,
   useActionData,
   redirect,
+  useSearchParams,
 } from "react-router";
 
 import classes from "./AuthForm.module.css";
 
-export default function LoginForm({ signup }) {
+export default function LoginForm() {
   const data = useActionData();
   const nav = useNavigate();
+  const navigation = useNavigation();
 
-  function handleSubmit() {
-    console.log("here");
-  }
+  const [searchParams] = useSearchParams();
 
-  function handleCancel() {
-    nav("..");
-  }
+  const isLogin = searchParams.get("mode") === "login";
+  const isSubmitting = navigation.state === "submitting";
 
   return (
-    <Form method="POST" onSubmit={handleSubmit} className={classes.form}>
+    <Form method="post" className={classes.form}>
+      <h1>{isLogin ? "Log in" : "New User Sign Up"}</h1>
+      {!isLogin && (
+        <p>
+          <input
+            placeholder="email address"
+            id="email"
+            type="email"
+            name="email"
+            required
+          />
+        </p>
+      )}
       <p>
         <input
           placeholder="username"
@@ -40,7 +52,7 @@ export default function LoginForm({ signup }) {
           required
         />
       </p>
-      {signup && (
+      {!isLogin && (
         <p>
           <input
             placeholder="confirm password"
@@ -52,10 +64,12 @@ export default function LoginForm({ signup }) {
         </p>
       )}
       <div className={classes.actions}>
-        <button type="button" onClick={handleCancel}>
-          Cancel
+        <Link to={`?mode=${isLogin ? "register" : "login"}`}>
+          {isLogin ? "Register" : "Login"}
+        </Link>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : !isLogin ? "Register" : "Login"}
         </button>
-        <button>{signup ? "Register" : "Login"}</button>
       </div>
     </Form>
   );
